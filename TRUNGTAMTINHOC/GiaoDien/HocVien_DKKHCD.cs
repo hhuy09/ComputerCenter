7ded3ee8aa53d72cfc0b05eefcebc4b8c7a4f5a7
hhuy09 <honghuyqtak2018@gmail.com>
2021-06-22 20:25:10 +0700

    Hoan thanh chuc nang Dk KH-CD

--- TRUNGTAMTINHOC/GiaoDien/HocVien_DKKHCD.cs
+++ TRUNGTAMTINHOC/GiaoDien/HocVien_DKKHCD.cs
@@ -23,11 +23,38 @@ namespace TRUNGTAMTINHOC
         private void HocVien_DKKHCD_Load(object sender, EventArgs e)
         {
             label2.Text = HoTen;
-            DataTable dt = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
-            dataGridView1.DataSource = dt;
 
-            label25.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
-            label26.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            DataTable dt1 = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
+            dataGridView1.DataSource = dt1;
+            dataGridView1.Columns[2].Width = 220;
+            int count1 = dataGridView1.Rows.Count - 1;
+
+            if (count1 > 0)
+            {
+                label25.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+                label26.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label25.Text = "(none)";
+                label26.Text = "(none)";
+            }
+
+            DataTable dt2 = NghiepVu.HV_LCD.DanhSachDKCD(MaHV);
+            dataGridView2.DataSource = dt2;
+            dataGridView2.Columns[2].Width = 250;
+            int count2 = dataGridView2.Rows.Count - 1;
+
+            if(count2 > 0)
+            {
+                label31.Text = dataGridView2.Rows[0].Cells[2].Value.ToString();
+                label30.Text = dataGridView2.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label31.Text = "(none)";
+                label30.Text = "(none)";
+            }     
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -38,22 +65,33 @@ namespace TRUNGTAMTINHOC
                 NghiepVu.HV_KDT.DangKyKhoaHoc(MaHV, MaLopDT);
             }
 
-            if (checkBox2.Checked)
+            if (checkBox2.Checked == true)
             {
                 string MaLopDT = label21.Text;
                 NghiepVu.HV_KDT.DangKyKhoaHoc(MaHV, MaLopDT);
             }
 
-            if (checkBox3.Checked)
+            if (checkBox3.Checked == true)
             {
                 string MaLopDT = label22.Text;
                 NghiepVu.HV_KDT.DangKyKhoaHoc(MaHV, MaLopDT);
             }
 
-            DataTable dt = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
-            dataGridView1.DataSource = dt;
-            label25.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
-            label26.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            DataTable dt1 = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
+            dataGridView1.DataSource = dt1;
+            dataGridView1.Columns[2].Width = 220;
+            int count1 = dataGridView1.Rows.Count - 1;
+
+            if (count1 > 0)
+            {
+                label25.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+                label26.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label25.Text = "(none)";
+                label26.Text = "(none)";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -66,8 +104,8 @@ namespace TRUNGTAMTINHOC
             if (e.RowIndex >= 0)
             {
                 DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
-                label25.Text = row.Cells[3].Value.ToString();
-                label26.Text = row.Cells[2].Value.ToString();
+                label25.Text = row.Cells[2].Value.ToString();
+                label26.Text = row.Cells[1].Value.ToString();
             }
         }
 
@@ -76,15 +114,90 @@ namespace TRUNGTAMTINHOC
             string MaLopDT = label26.Text;
             NghiepVu.HV_KDT.HuyDangKyKhoaHoc(MaHV, MaLopDT);
 
-            DataTable dt = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
-            dataGridView1.DataSource = dt;
-            label25.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
-            label26.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            DataTable dt1 = NghiepVu.HV_KDT.DanhSachDKKH(MaHV);
+            dataGridView1.DataSource = dt1;
+            dataGridView1.Columns[2].Width = 220;
+            int count1 = dataGridView1.Rows.Count - 1;
+
+            if (count1 > 0)
+            {
+                label25.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+                label26.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label25.Text = "(none)";
+                label26.Text = "(none)";
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (checkBox6.Checked == true)
+            {
+                string MaLopCD = label27.Text;
+                NghiepVu.HV_LCD.DangKyChuyenDe(MaHV, MaLopCD);
+            }
 
+            if (checkBox5.Checked == true)
+            {
+                string MaLopCD = label28.Text;
+                NghiepVu.HV_LCD.DangKyChuyenDe(MaHV, MaLopCD);
+            }
+
+            if (checkBox4.Checked == true)
+            {
+                string MaLopCD = label29.Text;
+                NghiepVu.HV_LCD.DangKyChuyenDe(MaHV, MaLopCD);
+            }
+
+            DataTable dt2 = NghiepVu.HV_LCD.DanhSachDKCD(MaHV);
+            dataGridView2.DataSource = dt2;
+            dataGridView2.Columns[2].Width = 250;
+            int count2 = dataGridView2.Rows.Count - 1;
+
+            if (count2 > 0)
+            {
+                label31.Text = dataGridView2.Rows[0].Cells[2].Value.ToString();
+                label30.Text = dataGridView2.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label31.Text = "(none)";
+                label30.Text = "(none)";
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView2.Rows[e.RowIndex];
+                label31.Text = row.Cells[2].Value.ToString();
+                label30.Text = row.Cells[1].Value.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string MaLopCD = label30.Text;
+            NghiepVu.HV_LCD.HuyDangKyChuyenDe(MaHV, MaLopCD);
+
+            DataTable dt2 = NghiepVu.HV_LCD.DanhSachDKCD(MaHV);
+            dataGridView2.DataSource = dt2;
+            dataGridView2.Columns[2].Width = 250;
+            int count2 = dataGridView2.Rows.Count - 1;
+
+            if (count2 > 0)
+            {
+                label31.Text = dataGridView2.Rows[0].Cells[2].Value.ToString();
+                label30.Text = dataGridView2.Rows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                label31.Text = "(none)";
+                label30.Text = "(none)";
+            }
         }
     }
 }
